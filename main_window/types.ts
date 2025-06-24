b369d2a8502d1d53515702e70fe27f2273457231
Ross Eyre <rosseyre@gmail.com>
2025-06-24 10:51:43 +0800

    fix attempt

--- main_window/types.ts
+++ main_window/types.ts
@@ -1,6 +1,6 @@
 
 import { InstalledAppId } from "@holochain/client";
-import { ZomeCallUnsignedNapi } from "holochain-lair-signer";
+// import { ZomeCallUnsignedNapi } from "holochain-lair-signer";
 
 export interface LauncherEnvironment {
   APP_INTERFACE_PORT?: number;
@@ -9,7 +9,7 @@ export interface LauncherEnvironment {
 }
 
 export interface ElectronAPICalls {
-  signZomeCall(z: ZomeCallUnsignedNapi): any;
+  // signZomeCall(z: ZomeCallUnsignedNapi): any;
   handleStatus(callback: (e: Event, msg: string) => void): void;
   handleLauncherEnv(callback: (e: Event, env: LauncherEnvironment) => void): void;
 }
