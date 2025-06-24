35418ff873133bd49198c79c40c2b16c6cc55df6
Ross Eyre <rosseyre@gmail.com>
2025-06-24 10:50:14 +0800

    fix attempt

--- main_window/preload.ts
+++ main_window/preload.ts
@@ -1,11 +1,11 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
 import { contextBridge, ipcRenderer } from "electron";
-import { ZomeCallUnsignedNapi } from "holochain-lair-signer";
+// import { ZomeCallUnsignedNapi } from "holochain-lair-signer";
 
 contextBridge.exposeInMainWorld('electronAPI', {
-  signZomeCall: (zomeCall: ZomeCallUnsignedNapi) => 
-    ipcRenderer.invoke('sign-zome-call', zomeCall),
+  // signZomeCall: (zomeCall: ZomeCallUnsignedNapi) => 
+  //   ipcRenderer.invoke('sign-zome-call', zomeCall),
   handleStatus: (callback: any) => 
     ipcRenderer.on('update-status', callback),
   handleLauncherEnv: (callback: any) => 
